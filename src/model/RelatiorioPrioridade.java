ba40bf9b8f335c5418e29e060bdefb67fe5424ec
Massayoshii <Massayoshibando@gmail.com>
2026-05-25 13:50:31 -0300

    relatiorio de prioridade mudado para uma classe propia e removido como metood no trecho rodovia

---
+++ src/model/RelatiorioPrioridade.java
@@ -0,0 +1,25 @@
+package model;
+
+public class RelatiorioPrioridade {
+    public void gerarRelatiorio(TrechoRodovia [] trechos){
+        System.out.println("\n=== RELATORIO DE PRIORIDADE === ");
+        for (TrechoRodovia trecho : trechos){
+            System.out.println(trecho);
+
+            if(trecho.getNivelVegetacaoCm() >= 25){
+                System.out.println("\nPRIORIDADE ALTA -> Roçada mecanizada");
+
+                IntervencaoOperacional rocada = new RocadaMecanizada();
+                rocada.executarServico();
+            }else if (trecho.getNivelVegetacaoCm() >= 12){
+                System.out.println("\nPRIORIDADE MEDIA -> Pulverizacao");
+                IntervencaoOperacional pulverizacao = new Pulverizacao();
+                pulverizacao.executarServico();
+            }else {
+                System.out.println("\nSem necessidade de intervencao");
+            }
+            System.out.println();
+        }
+
+    }
+}
--- src/model/TrechoRodovia.java
+++ src/model/TrechoRodovia.java
@@ -60,27 +60,6 @@ public class TrechoRodovia implements MonitoravelViaIoT {
         }
     }
 
-    public void gerarRelatiorio(TrechoRodovia [] trechos){
-        System.out.println("\n=== RELATORIO DE PRIORIDADE === ");
-        for (TrechoRodovia trecho : trechos){
-            System.out.println(trecho);
-
-            if(trecho.getNivelVegetacaoCm() >= 25){
-                System.out.println("\nPRIORIDADE ALTA -> Roçada mecanizada");
-
-                IntervencaoOperacional rocada = new RocadaMecanizada();
-                rocada.executarServico();
-            }else if (trecho.getNivelVegetacaoCm() >= 12){
-                System.out.println("\nPRIORIDADE MEDIA -> Pulverizacao");
-                IntervencaoOperacional pulverizacao = new Pulverizacao();
-                pulverizacao.executarServico();
-            }else {
-                System.out.println("\nSem necessidade de intervencao");
-            }
-            System.out.println();
-        }
-
-    }
 
     public void setQuilometroInicial(double quilometroInicial) {
         if(quilometroInicial >= 0){
